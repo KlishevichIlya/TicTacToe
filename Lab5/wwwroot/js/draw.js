391c738b1b69740f8970a872e8d263fed4709bcd
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2020-12-27 00:06:46 +0300

    Painted table

---
+++ Lab5/wwwroot/js/draw.js
@@ -0,0 +1,22 @@
+﻿let cells = document.querySelectorAll('tbody td');
+console.log(cells);
+draw(cells);
+
+function draw(cells) {
+    let counter = 0;
+    for (let cell of cells) {
+        cell.addEventListener('click', function () {
+            this.innerHTML = ['X', 'O'][counter % 2];
+            counter++;
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
